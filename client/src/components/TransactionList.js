07123820ae973b1daac23e43af1ca84714930a46
mohit <karkarmohit@gmail.com>
2020-03-05 20:03:02 +0530

    changed TransactionList.js and README.md files

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -15,7 +15,7 @@ export const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+        {transactions.map(transaction => (<Transaction key={transaction._id} transaction={transaction} />))}
       </ul>
     </>
   )
